2ca6bb586d1212885c5ad0126f4ff3b40c536131
Keucilene Coelho Cortes <keucilenecortes@gmail.com>
2021-03-30 11:49:47 -0400

    Keucilene - Atividade Obrigatória 2 - Aula 1.2.2 e Aula 1.2.3
    Aula 1.2.2 Title e Aula 1.2.3 Title erro

--- keucilene/2-flutter-na-pratica/ola_mundo/lib/main.dart
+++ keucilene/2-flutter-na-pratica/ola_mundo/lib/main.dart
@@ -1,20 +1,27 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(AppWidget());
+  runApp(AppWidget(
+    tittle: 'Aula ADS Tittle',
+  ));
 }
 
 class AppWidget extends StatelessWidget {
+  final String tittle;
+  const AppWidget({Key key, this.tittle}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return Container(
       padding: EdgeInsets.all(30),
-      child: Text(
-        'Aula ADS Mobile',
-        textDirection: TextDirection.ltr,
-        style: TextStyle(
-          color: Colors.white,
-          fontSize: 100.00,
+      child: Center(
+        child: Text(
+          'Aula ADS Mobile',
+          textDirection: TextDirection.ltr,
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 40.00,
+          ),
         ),
       ),
     );
