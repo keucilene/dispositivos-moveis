06f9008b732cca38f5872e5159df959cd3bfe9bd
Keucilene Coelho Cortes <keucilenecortes@gmail.com>
2021-03-30 11:30:52 -0400

    Keucilene - Atividade Obrigatória 2 - Aula 1.1.1
    Aula 1.1.1 Primeiros Passos

---
+++ keucilene/2-flutter-na-pratica/ola_mundo/lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    Container(
+      padding: EdgeInsets.all(30),
+      child: Text(
+        'Aula ADS Mobile',
+        textDirection: TextDirection.ltr,
+        style: TextStyle(
+          color: Colors.white, 
+          fontSize: 100,
+        ),
+      ),
+    ),
+  );
+}
