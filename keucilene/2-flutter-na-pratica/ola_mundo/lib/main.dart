17f97a152749248a6006ba0a28c8529479a558b5
Keucilene Coelho Cortes <keucilenecortes@gmail.com>
2021-03-30 11:36:04 -0400

    Keucilene - Atividade Obrigatória 2 - Aula 1.2
    Aula 1.2 Stateless

--- keucilene/2-flutter-na-pratica/ola_mundo/lib/main.dart
+++ keucilene/2-flutter-na-pratica/ola_mundo/lib/main.dart
@@ -1,17 +1,22 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    Container(
+  runApp(AppWidget());
+}
+
+class AppWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
       padding: EdgeInsets.all(30),
       child: Text(
         'Aula ADS Mobile',
         textDirection: TextDirection.ltr,
         style: TextStyle(
-          color: Colors.white, 
-          fontSize: 100,
+          color: Colors.white,
+          fontSize: 100.00,
         ),
       ),
-    ),
-  );
+    );
+  }
 }
