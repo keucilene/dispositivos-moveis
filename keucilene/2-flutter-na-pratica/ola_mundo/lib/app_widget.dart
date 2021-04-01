6483ef418f50342114c91633e7d6788f09f339d4
Keucilene Coelho Cortes <keucilenecortes@gmail.com>
2021-04-01 15:05:47 -0400

    Keucilene - Atividade Obrigatória 2 - Aula 1.4
    Aula 1.4 - Organizando o Código

---
+++ keucilene/2-flutter-na-pratica/ola_mundo/lib/app_widget.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'home_page.dart';
+
+class AppWidget extends StatelessWidget {
+  final String tittle;
+  const AppWidget({Key key, this.tittle}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(primaryColor: Colors.green),
+      home: HomePage(),
+    );
+  }
+}
--- keucilene/2-flutter-na-pratica/ola_mundo/lib/main.dart
+++ keucilene/2-flutter-na-pratica/ola_mundo/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'app_widget.dart';
 
 void main() {
   runApp(AppWidget(
@@ -6,43 +7,3 @@ void main() {
   ));
 }
 
-class AppWidget extends StatelessWidget {
-  final String tittle;
-  const AppWidget({Key key, this.tittle}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      theme: ThemeData(primaryColor: Colors.green),
-      home: HomePage(),
-    );
-  }
-}
-
-class HomePage extends StatefulWidget {
-  @override
-  State<HomePage> createState() {
-    return HomePageState();
-  }
-}
-
-class HomePageState extends State<HomePage> {
-  int counter = 0;
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      child: Center(
-        child: GestureDetector(
-          child: Text('Clique: $counter'),
-          onTap: () {
-            setState(() {
-              counter++;
-              print(counter);
-            });
-          },
-        ),
-      ),
-    );
-  }
-}
