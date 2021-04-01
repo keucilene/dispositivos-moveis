02e6089b5278daab3e472f3bc687a2374e4e4e2f
Keucilene Coelho Cortes <keucilenecortes@gmail.com>
2021-04-01 17:41:56 -0400

    Keucilene - Atividade Obrigatória 2 - Aula 1.12.3
    Aula 1.12.3 Logica Login

--- keucilene/2-flutter-na-pratica/ola_mundo/lib/login_page.dart
+++ keucilene/2-flutter-na-pratica/ola_mundo/lib/login_page.dart
@@ -6,38 +6,51 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPageState extends State<LoginPage> {
+  String email = "";
+  String password = "";
+
   @override
   Widget build(BuildContext context) {
-    return Material(
-      child: SizedBox(
-        width: double.infinity,
-        height: double.infinity,
-        child: Padding(
-          padding: const EdgeInsets.all(30),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              TextField(
-                keyboardType: TextInputType.emailAddress,
-                decoration: InputDecoration(
-                  labelText: 'E-mail',
-                  border: OutlineInputBorder(),
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: SizedBox(
+          width: MediaQuery.of(context).size.width,
+          height: MediaQuery.of(context).size.height,
+          child: Padding(
+            padding: const EdgeInsets.all(30),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                TextField(
+                  onChanged: (text) => email = text,
+                  keyboardType: TextInputType.emailAddress,
+                  decoration: InputDecoration(
+                    labelText: 'E-mail',
+                    border: OutlineInputBorder(),
+                  ),
                 ),
-              ),
-              SizedBox(height: 15),
-              TextField(
-                obscureText: true,
-                decoration: InputDecoration(
-                  labelText: 'Senha',
-                  border: OutlineInputBorder(),
+                SizedBox(height: 15),
+                TextField(
+                  onChanged: (text) => password = text,
+                  obscureText: true,
+                  decoration: InputDecoration(
+                    labelText: 'Senha',
+                    border: OutlineInputBorder(),
+                  ),
                 ),
-              ),
-              SizedBox(height: 20),
-              ElevatedButton(
-                onPressed: () {},
-                child: Text('Entrar'),
-              ),
-            ],
+                SizedBox(height: 20),
+                ElevatedButton(
+                  onPressed: () {
+                    if (email == 'keucilene@email.com' && password == '123') {
+                      print('Login correto');
+                    } else {
+                      print('Login errado');
+                    }
+                  },
+                  child: Text('Entrar'),
+                ),
+              ],
+            ),
           ),
         ),
       ),
